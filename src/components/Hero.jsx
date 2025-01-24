17ed62896b2fc5c1172d526f5a81d87acb2fe6db
Sadok <kouassisadok9@gmail.com>
2025-01-24 14:19:36 +0000

    bn

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { Download } from "lucide-react";
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import G from "../assets/g.jpg";
+import CV from "../assets/cv.pdf";
 import { motion } from "framer-motion";
 import "../Css/Hero.css";
 
@@ -46,19 +46,21 @@ const Hero = () => {
             className="lg:w-[600px] mb-4 md:mb-0 text-sm text-gray-300"
           >
             Je suis développeur full stack, passionné par la création
-            d'applications web modernes et performantes. Mon expertise englobe à
-            la fois le développement front-end et back-end, me permettant de
-            concevoir des solutions complètes et adaptées aux besoins de mes
-            clients.
+            d&apos;applications web modernes et performantes. Mon expertise
+            englobe à la fois le développement front-end et back-end, me
+            permettant de concevoir des solutions complètes et adaptées aux
+            besoins de mes clients.
           </motion.p>
-          <motion.button
-            initial={{ opacity: 0, x: -100 }}
-            whileInView={{ opacity: 1, x: 0 }}
-            transition={{ duration: 1.4, delay: 0.5 }}
-            className="px-3 py-2 flex items-center gap-2 bg-blue-500 rounded-md text-white hover:bg-blue-600 shadow-blue-500 border-2 border-blue-400 transition-all shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#33CCCC,0_0_15px_#33CCCC,0_0_30px_#33CCCC] "
-          >
-            <Download className="w-5 h-5" /> Télécharger le CV
-          </motion.button>
+          <a href={CV} download="cv">
+            <motion.button
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1.4, delay: 0.5 }}
+              className="px-3 py-2 flex items-center gap-2 bg-blue-500 rounded-md text-white hover:bg-blue-600 shadow-blue-500 border-2 border-blue-400 transition-all shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#33CCCC,0_0_15px_#33CCCC,0_0_30px_#33CCCC] "
+            >
+              <Download className="w-5 h-5" /> Télécharger le CV
+            </motion.button>
+          </a>
           <div className="flex gap-3 text-2xl transition-all mt-5 md:mt-0">
             <a href="https://www.facebook.com/share/1DTGrG5Hhg/?mibextid=wwXIfr">
               <FaFacebook className="hover:text-blue-400" />
