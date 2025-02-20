6045fe02b203073a0adf7b1d8360b604bd4f8622
Sadok <kouassisadok9@gmail.com>
2025-02-20 02:12:27 +0100

    vb

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import Swal from "sweetalert2";
 import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
 const Contact = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
